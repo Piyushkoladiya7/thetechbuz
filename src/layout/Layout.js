6848970acca1ca3c559e637ea63abda07005a5f0
mitesh <miteshpatel.7seasol@gmail.com>
2022-04-15 17:54:33 +0530

    removed header and footer from quiz component

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -4,9 +4,11 @@ import Header from './header/Header'
 import $ from "jquery";
 import "../pages/jq/jq.js";
 import { Button, Container } from 'react-bootstrap';
+import { useLocation } from 'react-router-dom';
 
 const Layout = ({ children, ...props }) => {
 
+  const location = useLocation()
   const [top, setTop] = useState(false);
   useEffect(() => {
     $(document).ready(function () {
@@ -16,12 +18,17 @@ const Layout = ({ children, ...props }) => {
 
   return (
     <>
-      <Container>
-        <Header />
+      {location.pathname !== "/quiz" ?
+        <>
+          <Container>
+            <Header />
+            <div {...props}>{children}</div>
+            <button className="return-to-top" onClick={() => setTop(!top)}> <img src='./assets/image/back_to_top.png' alt="" /></button>
+          </Container>
+          <Footer />
+        </> :
         <div {...props}>{children}</div>
-        <button className="return-to-top" onClick={() => setTop(!top)}> <img src='./assets/image/back_to_top.png' alt="" /></button>
-      </Container>
-      <Footer />
+      }
     </>
   )
 }
